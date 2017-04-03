813ec397ed7b2c1c4312ba299824f03c29541eef
Nishank <fishtankc@gmail.com>
2017-04-03 16:38:33 +0100

    4
    4

---
+++ Week 2/notes.js
@@ -0,0 +1,23 @@
+DOM - document object model
+document - the webpage file
+object - everything that can be treated as an individual piece
+model - the structure
+
+document.getElementById
+document.getElementsByTagName
+document.querySelectorAll
+
+myElement.onclick
+button.addEventListener('click', sumFunc, false);
+
+
+Pillars of JS
+Sequence - program flow
+
+Selection - conditions, asking questions
+
+Iteration - loops
+
+var cars = ['bmw', 'porch', 'ford'];
+
+var cars = new Array("bmw","porch", "ford");
